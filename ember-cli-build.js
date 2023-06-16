30ea00656264d0ce389a8859effff01f1800af41
Kirill Shitikov <k.shitikov@smartweb.com.ua>
2023-06-16 09:26:44 +0300

    Add style

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,10 +5,13 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     prember: {
-      urls: [
-        '/',
-      ]
-    }
+      urls: ['/'],
+    },
+
+    'ember-bootstrap': {
+      bootstrapVersion: 4,
+      importBootstrapCSS: false,
+    },
   });
 
   return app.toTree();
