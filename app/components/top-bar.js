2a3a99e52c9006815cb2ebca7d17d411fedb4616
Kirill Shitikov <k.shitikov@smartweb.com.ua>
2023-06-26 11:28:58 +0300

    Refactor project

--- app/components/top-bar.js
+++ app/components/top-bar.js
@@ -1,21 +1,6 @@
 import Component from '@glimmer/component';
+import { socials } from '../data/socials';
 
 export default class TopBarComponent extends Component {
-  socials = [
-    {
-      url: '#',
-      slag: 'facebook',
-      name: 'Facebook',
-    },
-    {
-      url: '#',
-      slag: 'twitter',
-      name: 'Twitter',
-    },
-    {
-      url: '#',
-      slag: 'instagram',
-      name: 'Instagram',
-    },
-  ];
+  socials = socials;
 }
