1300e6e374515225c5b7068c628737faba02039e
Kirill Shitikov <k.shitikov@smartweb.com.ua>
2023-06-26 11:34:50 +0300

    Refactor

---
+++ app/data/socials.js
@@ -0,0 +1,17 @@
+export const socials = [
+  {
+    url: '#',
+    slag: 'facebook',
+    name: 'Facebook',
+  },
+  {
+    url: '#',
+    slag: 'twitter',
+    name: 'Twitter',
+  },
+  {
+    url: '#',
+    slag: 'instagram',
+    name: 'Instagram',
+  },
+];
